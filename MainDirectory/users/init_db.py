0f8c5d6080082f78a651808da1243f2d1deb88e3
VladilavDiD <vladdidyk14@gmail.com>
2025-11-19 12:25:48 +0200

    push

--- MainDirectory/users/init_db.py
+++ MainDirectory/users/init_db.py
@@ -1,6 +1,6 @@
 from db import init_db
 
 if __name__ == "__main__":
-    print("Starting init_db...")  # Додайте для тесту
+    print("Starting init_db...")
     init_db()
     print("DB created!")
